2c0c47e5a11a3c865819d0c0fcc24334d056ea11
Your Name <you@example.com>
2019-07-31 16:58:52 -0300

    add new files to test deploy

---
+++ precache-manifest.48f5f1593765e5a21a99f54becacce91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e331a4572950000456b7fffc7cb9e5",
+    "url": "/test-techcamp/index.html"
+  },
+  {
+    "revision": "7fff14343f8ab6f35984",
+    "url": "/test-techcamp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "afb77b19a2d1f770e44b",
+    "url": "/test-techcamp/static/js/2.41b474c4.chunk.js"
+  },
+  {
+    "revision": "7fff14343f8ab6f35984",
+    "url": "/test-techcamp/static/js/main.db5ff9ff.chunk.js"
+  },
+  {
+    "revision": "2ad84c16c95ea0e1d455",
+    "url": "/test-techcamp/static/js/runtime~main.49a02c1a.js"
+  }
+]);
